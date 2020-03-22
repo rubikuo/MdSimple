eb6bd32d14b5cd21b3710de5e7fbbe20421ef556
Rubi Kuo <rubitiara@gmail.com>
2020-03-22 08:35:53 +0100

    fixed switch checkbox hover and focus

--- src/components/switch/Switch.js
+++ src/components/switch/Switch.js
@@ -1,27 +1,46 @@
 import React from 'react';
 import './Switch.scss';
 
-const Switch = ({modeSwitch, handleMode}) => {
+const Switch = ({ modeSwitch, handleMode }) => {
 	return (
 		<div className="switch">
-			<label htmlFor="mode" className="switch__label">
-                <span 
-                className="switch__title"> Dark Mode: <b>{ modeSwitch ? "ON":"OFF" }</b></span>
-                <input 
-                type="checkbox"
-                checked={modeSwitch}
-                onChange={handleMode}
-                name ="mode"
-                id="mode"
-                className="switch__input" />
+			<span className="switch__title">
+				Dark Mode: <b>{modeSwitch ? 'ON' : 'OFF'}</b>
+			</span>
+			<label htmlFor="enabled" className="switch__label">
+				<input
+					type="checkbox"
+					checked={modeSwitch}
+					onChange={handleMode}
+					name="enabled"
+					id="enabled"
+					className="switch__input switch__input--enabled"
+				/>
 				<span className="switch--fake" />
 			</label>
-			<br />
-			<label className="switch__label">
-                <input type="checkbox" 
-                className="switch__input" 
-                name="drag"
-                />
+
+			<span className="switch__title">Disabled: OFF</span>
+			<label htmlFor="disabled" className="switch__label">
+				<input
+					type="checkbox"
+					disabled="disabled"
+					name="disabled"
+					id="disabled"
+					checked="checked"
+					className="switch__input switch__input--disabled"
+				/>
+				<span className="switch--fake" />
+			</label>
+            <span className="switch__title">
+					dragable: OFF
+				</span>
+			<label htmlFor="dragable" className="switch__label">
+				<input
+					type="checkbox"
+					name="dragable"
+					id="dragable"
+					className="switch__input switch__input--dragable"
+				/>
 				<span className="switch--fake" />
 			</label>
 		</div>
