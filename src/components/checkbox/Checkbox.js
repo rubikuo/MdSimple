867348c39263b8a253323a1a788564547ae3a671
Rubi Kuo <rubitiara@gmail.com>
2020-03-23 19:05:14 +0100

    added css to checkbox but not working, made initial radioBtn

--- src/components/checkbox/Checkbox.js
+++ src/components/checkbox/Checkbox.js
@@ -1,25 +1,19 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './Checkbox.scss';
 
-
-
 const Checkbox = () => {
 	const [ options, setOptions ] = useState([
 		{ country: 'Japan', id: 1, checked: false },
 		{ country: 'Korea', id: 2, checked: false },
 		{ country: 'Singapore', id: 3, checked: false },
 		{ country: 'Taiwan', id: 4, checked: false }
-    ]);
+	]);
 
-    const parentRef = useRef();
-
-    
+	const parentRef = useRef();
 
 	const handleAllChecked = (e) => {
 		let copyOptions = [ ...options ];
-		console.log(copyOptions);
 		copyOptions.forEach((place) => (place.checked = e.target.checked));
-		console.log(copyOptions);
 		setOptions(copyOptions);
 	};
 
@@ -28,61 +22,68 @@ const Checkbox = () => {
 		copyOptions.forEach((opt) => {
 			if (opt.country === e.target.value) opt.checked = e.target.checked;
 		});
-        setOptions(copyOptions);
-        let checkedOptions = [];
-        copyOptions.forEach((opt)=>{
-            if(opt.checked){
-                checkedOptions.push(opt)
-            }
-        })
+		setOptions(copyOptions);
+		let checkedOptions = [];
+		copyOptions.forEach((opt) => {
+			if (opt.checked) {
+				checkedOptions.push(opt);
+			}
+		});
 
-        let checkedCount = checkedOptions.length;
-        let totalCount = options.length;
-        console.log(checkedCount, totalCount);
-        if(checkedCount < totalCount && checkedCount >0){
-            parentRef.current.indeterminate = true;
-        }else if(checkedCount=== totalCount){
-            parentRef.current.checked = true;
-            parentRef.current.indeterminate = false;
-        }else{
-            parentRef.current.indeterminate = false;
-            parentRef.current.checked = false;
-        }
-        
-    };
-    
-   
+		let checkedCount = checkedOptions.length;
+		let totalCount = options.length;
+		//console.log(checkedCount, totalCount);
+		if (checkedCount < totalCount && checkedCount > 0) {
+			parentRef.current.indeterminate = true;
+		} else if (checkedCount === totalCount) {
+			parentRef.current.checked = true;
+			parentRef.current.indeterminate = false;
+		} else {
+			parentRef.current.indeterminate = false;
+			parentRef.current.checked = false;
+		}
+	};
 
 	return (
 		<div className="checkbox">
-			<input
-				type="checkbox"
-				id="option"
-				ref={parentRef}
-                onChange={handleAllChecked}
-              
-				className="checkbox__input checkbox__input--main"
-			/>
-			<label htmlFor="option">All</label>
-			<ul>
+            <label htmlFor="option" className="checkbox__label checkbox__label--parent">
+      
+				<input
+					type="checkbox"
+					id="option"
+					ref={parentRef}
+					onChange={handleAllChecked}
+					className="checkbox__input checkbox__input--main"
+				/>
+                
+				<span className="checkbox--fake"/>
+                {/* <span>All</span> */}
+			</label>
+    
+			<ul className="checkbox__list">
 				{options.map((opt, i) => {
 					return (
-						<li key={opt.id}>
-							<label htmlFor={opt.country}>
+						<li key={opt.id} className="checkbox__list--item">
+							<label htmlFor={opt.country} className="checkbox__label checkbox__label--subOption">
 								<input
 									type="checkbox"
 									name={opt.country}
-									className="checkbox__input checkbox__input--subOption"
+									className="checkbox__input checkbox__input--subOpt"
 									checked={opt.checked}
 									onChange={handleOptions}
 									value={opt.country}
 								/>
-								{opt.country}
+								<span className="checkbox--fake"/>
+								{/* <span>{opt.country}</span> */}
 							</label>
 						</li>
 					);
 				})}
 			</ul>
+            {/* <label htmlFor="disabled" className="checkbox__label--disabled">
+            <input type="checkbox" disabled className=" checkbox__input checkbox__input--disabled"/>
+            Disabled
+            </label> */}
 		</div>
 	);
 };
--- src/components/radiobtn/RadioBtn.js
+++ src/components/radiobtn/RadioBtn.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React from 'react';
 
-const RadioBtn = () =>{
-   
-return(
-    <>
-    </>
+const RadioBtn = () => {
+	const datas = [ 'Female', 'Male', 'Other', 'Disabled' ];
 
-)
-
-
-}
+	return (
+		<div className="radio">
+			<span>Gender</span>
+			{datas.map((data) => {
+				return (
+					<div className="radio__ctn" key={data}>
+						<input
+							type="radio"
+							className="radio__input"
+							name="gender"
+							id={data}
+							value={data}
+							disabled={data === 'Disabled' ? true : false}
+						/>
+						<span className="radio--fake" />
+						<label htmlFor={data}>{data === 'Disabled' ? 'Disabled option' : data}</label>
+					</div>
+				);
+			})}
+		</div>
+	);
+};
 
 export default RadioBtn;
